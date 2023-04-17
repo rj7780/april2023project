80ecc30edf8c4535d13244ac2c5ff93d0395b24d
rakesh jadhav <130731777+rj7780@users.noreply.github.com>
2023-04-17 11:29:48 +0530

    Create github.java

--- april2023project/src/april2023project/github.java
+++ april2023project/src/april2023project/github.java
@@ -4,9 +4,9 @@ public class github {
   public static void main(String[] args) {
 	System.out.println( "hello girthub");
 	System.out.println( "ok done it");
-}
-  
-  
+  	System.out.println( " new changes in masater");
+
+  }  
   
   
   
